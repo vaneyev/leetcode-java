14899cd1942a46ca4b8b475b2e50995b11c011f6
Александр Ванеев <a.vaneev@viscur.ru>
2023-11-10 16:45:28 +0300

    237. Delete Node in a Linked List

---
+++ src/main/java/org/example/Solution237.java
@@ -0,0 +1,20 @@
+package org.example;
+
+/**
+ * Delete Node in a Linked List
+ */
+public class Solution237 {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/test/java/org/example/Solution237Test.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution237Test {
+
+    @Test
+    void deleteNode1() {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+        new Solution237().deleteNode(head.next);
+        assertEquals(4, head.val);
+        assertEquals(1, head.next.val);
+        assertEquals(9, head.next.next.val);
+        assertNull(head.next.next.next);
+    }
+
+    @Test
+    void deleteNode2() {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+        new Solution237().deleteNode(head.next.next);
+        assertEquals(4, head.val);
+        assertEquals(5, head.next.val);
+        assertEquals(9, head.next.next.val);
+        assertNull(head.next.next.next);
+    }
+}
